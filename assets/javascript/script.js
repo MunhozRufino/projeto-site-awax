3cee60ac5d5b1cb9d00c967df4a10aeeee7e07e1
Optivision Licitacao <munhozrufino.programacao@outlook.com>
2022-07-27 17:03:40 -0300

    Ajuste de menu

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+function clickArea() {
+    let clickArea = document.getElementById('menu');
+
+        if(clickArea.style.display =='none') {
+            clickArea.style.display ='block'
+        } else {
+            clickArea.style.display ='none';
+        }
+
+
+}
